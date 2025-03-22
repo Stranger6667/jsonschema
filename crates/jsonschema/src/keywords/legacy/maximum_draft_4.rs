06538c8e05d7b758c5c2d73af57f3a7bdfb6eb07
Dmitry Dygalo <dmitry@dygalo.dev>
2025-03-22 17:33:11 +0100

    wip
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- crates/jsonschema/src/keywords/legacy/maximum_draft_4.rs
+++ crates/jsonschema/src/keywords/legacy/maximum_draft_4.rs
@@ -1,6 +1,14 @@
 use crate::{
     compiler,
     keywords::{minmax, CompilationResult},
+    TracingCallback, TracingContext,
+};
+use crate::{
+    error::ValidationError,
+    ext::numeric,
+    paths::{LazyLocation, Location},
+    types::JsonType,
+    validator::Validate,
 };
 use serde_json::{Map, Value};
 
@@ -11,8 +19,173 @@ pub(crate) fn compile<'a>(
     schema: &'a Value,
 ) -> Option<CompilationResult<'a>> {
     if let Some(Value::Bool(true)) = parent.get("exclusiveMaximum") {
-        minmax::compile_exclusive_maximum(ctx, parent, schema)
+        compile_exclusive(ctx, parent, schema)
     } else {
         minmax::compile_maximum(ctx, parent, schema)
+    }
+}
+
+pub(crate) struct ExclusiveMaximumU64Validator {
+    limit: u64,
+    limit_val: Value,
+    location: Location,
+    maximum_location: Location,
+}
+pub(crate) struct ExclusiveMaximumI64Validator {
+    limit: i64,
+    limit_val: Value,
+    location: Location,
+    maximum_location: Location,
+}
+pub(crate) struct ExclusiveMaximumF64Validator {
+    limit: f64,
+    limit_val: Value,
+    location: Location,
+    maximum_location: Location,
+}
+
+macro_rules! validate {
+    ($validator: ty) => {
+        impl Validate for $validator {
+            fn validate<'i>(
+                &self,
+                instance: &'i Value,
+                location: &LazyLocation,
+            ) -> Result<(), ValidationError<'i>> {
+                if self.is_valid(instance) {
+                    Ok(())
+                } else {
+                    Err(ValidationError::exclusive_maximum(
+                        self.location.clone(),
+                        location.into(),
+                        instance,
+                        self.limit_val.clone(),
+                    ))
+                }
+            }
+
+            fn is_valid(&self, instance: &Value) -> bool {
+                if let Value::Number(item) = instance {
+                    numeric::lt(item, self.limit)
+                } else {
+                    true
+                }
+            }
+            fn matches_type(&self, instance: &Value) -> bool {
+                matches!(instance, Value::Number(_))
+            }
+            fn schema_path(&self) -> &Location {
+                &self.location
+            }
+            fn trace(
+                &self,
+                instance: &Value,
+                location: &LazyLocation,
+                callback: TracingCallback<'_>,
+            ) -> bool {
+                let result = self.is_valid(instance);
+                let rv = if self.matches_type(instance) {
+                    Some(result)
+                } else {
+                    None
+                };
+                TracingContext::new(location, self.schema_path(), rv).call(callback);
+                TracingContext::new(location, &self.maximum_location, rv).call(callback);
+                result
+            }
+        }
+    };
+}
+
+validate!(ExclusiveMaximumU64Validator);
+validate!(ExclusiveMaximumI64Validator);
+
+impl Validate for ExclusiveMaximumF64Validator {
+    fn is_valid(&self, instance: &Value) -> bool {
+        if let Value::Number(item) = instance {
+            numeric::lt(item, self.limit)
+        } else {
+            true
+        }
+    }
+
+    fn validate<'i>(
+        &self,
+        instance: &'i Value,
+        location: &LazyLocation,
+    ) -> Result<(), ValidationError<'i>> {
+        if self.is_valid(instance) {
+            Ok(())
+        } else {
+            Err(ValidationError::exclusive_maximum(
+                self.location.clone(),
+                location.into(),
+                instance,
+                self.limit_val.clone(),
+            ))
+        }
+    }
+    fn matches_type(&self, instance: &Value) -> bool {
+        matches!(instance, Value::Number(_))
+    }
+    fn schema_path(&self) -> &Location {
+        &self.location
+    }
+    fn trace(
+        &self,
+        instance: &Value,
+        location: &LazyLocation,
+        callback: TracingCallback<'_>,
+    ) -> bool {
+        let result = self.is_valid(instance);
+        let rv = if self.matches_type(instance) {
+            Some(result)
+        } else {
+            None
+        };
+        TracingContext::new(location, self.schema_path(), rv).call(callback);
+        TracingContext::new(location, &self.maximum_location, rv).call(callback);
+        result
+    }
+}
+
+#[inline]
+pub(crate) fn compile_exclusive<'a>(
+    ctx: &compiler::Context,
+    _: &'a Map<String, Value>,
+    schema: &'a Value,
+) -> Option<CompilationResult<'a>> {
+    if let Value::Number(limit) = schema {
+        let location = ctx.location().join("exclusiveMaximum");
+        if let Some(limit) = limit.as_u64() {
+            Some(Ok(Box::new(ExclusiveMaximumU64Validator {
+                limit,
+                limit_val: (*schema).clone(),
+                location,
+                maximum_location: ctx.location().join("maximum"),
+            })))
+        } else if let Some(limit) = limit.as_i64() {
+            Some(Ok(Box::new(ExclusiveMaximumI64Validator {
+                limit,
+                limit_val: (*schema).clone(),
+                location,
+                maximum_location: ctx.location().join("maximum"),
+            })))
+        } else {
+            let limit = limit.as_f64().expect("Always valid");
+            Some(Ok(Box::new(ExclusiveMaximumF64Validator {
+                limit,
+                limit_val: (*schema).clone(),
+                location,
+                maximum_location: ctx.location().join("maximum"),
+            })))
+        }
+    } else {
+        Some(Err(ValidationError::single_type_error(
+            Location::new(),
+            ctx.location().clone(),
+            schema,
+            JsonType::Number,
+        )))
     }
 }
