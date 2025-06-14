5e3a3bf23830fca63080da9fb17e782bb6ad80a1
Dmitry Dygalo <dmitry@dygalo.dev>
2025-06-14 22:14:13 +0200

    wip
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- crates/jsonschema/src/compiler_v2.rs
+++ crates/jsonschema/src/compiler_v2.rs
@@ -43,36 +43,33 @@ pub fn build(mut config: ValidationOptions, schema: &Value) -> Validator {
     compile_impl(&schema)
 }
 
-// The idea is that a JSON Schema is stored as a tree in a flat layout.
-//
-// Each subschema (including the root one) may contain:
-//   - keywords (like `properties` or `minLength`)
-//   - annotations (like `description`) - TODO
-//
-// There are three allocations, one for schemas and one for each of the entities above.
-// Then schemas refer to them via ranges of IDs
-
 /// Context for backpatching of applicator nodes.
+#[derive(Debug)]
 struct PendingPatch {
-    id: usize,
+    schema_id: SchemaId,
+    keyword_index: usize,
     kind: PatchKind,
 }
 
 impl PendingPatch {
-    fn properties(id: usize, node_id: ir::NodeId) -> Self {
+    fn properties(schema_id: SchemaId, keyword_index: usize, node_id: ir::NodeId) -> Self {
         PendingPatch {
-            id,
+            schema_id,
+            keyword_index,
             kind: PatchKind::Properties { node_id },
         }
     }
-    fn r#ref(id: usize, node_id: ir::NodeId) -> Self {
+
+    fn r#ref(schema_id: SchemaId, keyword_index: usize, node_id: ir::NodeId) -> Self {
         PendingPatch {
-            id,
+            schema_id,
+            keyword_index,
             kind: PatchKind::Ref { node_id },
         }
     }
 }
 
+#[derive(Debug)]
 enum PatchKind {
     Properties { node_id: ir::NodeId },
     Ref { node_id: ir::NodeId },
@@ -117,7 +114,8 @@ fn compile_impl<'a>(schema: &ir::SchemaIR<'a>) -> Validator {
                 let schema_id = SchemaId(validator.schemas.len());
                 node_to_schema.insert(node_id, schema_id);
 
-                let keywords_start = validator.keywords.len();
+                let mut keywords = Vec::new();
+
                 for child_id in schema.children(node_id) {
                     let node = &schema[child_id];
                     if let Some(EdgeLabel::Key(key)) = node.label {
@@ -125,18 +123,20 @@ fn compile_impl<'a>(schema: &ir::SchemaIR<'a>) -> Validator {
                             "maxLength" => {
                                 if let IRValue::Number(number) = node.value {
                                     if let Some(limit) = number.as_u64() {
-                                        validator.push_keyword(Keyword::MaxLength {
+                                        keywords.push(Keyword::MaxLength {
                                             limit: limit as usize,
                                         });
                                     }
                                 }
                             }
                             "properties" => {
+                                let keyword_index = keywords.len();
                                 pending_patches.push(PendingPatch::properties(
-                                    validator.keywords.len(),
+                                    schema_id,
+                                    keyword_index,
                                     child_id,
                                 ));
-                                validator.push_keyword(Keyword::properties());
+                                keywords.push(Keyword::properties());
                                 for id in schema.children(child_id) {
                                     queue.push(id);
                                 }
@@ -145,18 +145,20 @@ fn compile_impl<'a>(schema: &ir::SchemaIR<'a>) -> Validator {
                                 let ir::IRValue::Reference(target_id) = node.value else {
                                     panic!()
                                 };
-                                pending_patches
-                                    .push(PendingPatch::r#ref(validator.keywords.len(), target_id));
-                                validator.push_keyword(Keyword::r#ref());
+                                let keyword_index = keywords.len();
+                                pending_patches.push(PendingPatch::r#ref(
+                                    schema_id,
+                                    keyword_index,
+                                    target_id,
+                                ));
+                                keywords.push(Keyword::r#ref());
                                 queue.push(target_id);
                             }
                             _ => {}
                         }
                     }
                 }
-                let schema = Schema {
-                    keywords: keywords_start..validator.keywords.len(),
-                };
+                let schema = Schema { keywords };
 
                 if node_id == ir::NodeId::root_id() {
                     validator.root = schema.clone();
@@ -178,9 +180,24 @@ fn apply_patch(
     patch: PendingPatch,
     node_to_schema: &AHashMap<ir::NodeId, SchemaId>,
 ) {
+    let target = if patch.schema_id.0 == 0 {
+        &mut validator.root
+    } else {
+        &mut validator.schemas[patch.schema_id.0]
+    };
+
+    apply_patch_impl(target, schema, &patch, node_to_schema);
+}
+
+fn apply_patch_impl(
+    target: &mut Schema,
+    schema: &ir::SchemaIR,
+    patch: &PendingPatch,
+    node_to_schema: &AHashMap<ir::NodeId, SchemaId>,
+) {
     match patch.kind {
         PatchKind::Properties { node_id } => {
-            if let Keyword::Properties { properties } = &mut validator.keywords[patch.id] {
+            if let Keyword::Properties { properties } = &mut target.keywords[patch.keyword_index] {
                 for property_id in schema.children(node_id) {
                     let property = &schema[property_id];
                     if let Some(EdgeLabel::Key(key)) = property.label {
@@ -191,7 +208,7 @@ fn apply_patch(
             }
         }
         PatchKind::Ref { node_id } => {
-            if let Keyword::Ref { schema_id } = &mut validator.keywords[patch.id] {
+            if let Keyword::Ref { schema_id } = &mut target.keywords[patch.keyword_index] {
                 *schema_id = node_to_schema[&node_id];
             }
         }
@@ -202,20 +219,20 @@ fn apply_patch(
 pub struct Validator {
     root: Schema,
     schemas: Vec<Schema>,
-    keywords: Vec<Keyword>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Schema {
-    keywords: Range<usize>,
+    keywords: Vec<Keyword>,
 }
 
 impl Validator {
     pub fn new() -> Self {
         Validator {
-            root: Schema { keywords: 0..0 },
+            root: Schema {
+                keywords: Vec::new(),
+            },
             schemas: Vec::new(),
-            keywords: Vec::new(),
         }
     }
 
@@ -223,12 +240,8 @@ impl Validator {
         self.schemas.push(schema);
     }
 
-    pub fn push_keyword(&mut self, keyword: Keyword) {
-        self.keywords.push(keyword);
-    }
-
     pub fn is_valid(&self, value: &Value) -> bool {
-        for keyword in &self.keywords[self.root.keywords.start..self.root.keywords.end] {
+        for keyword in &self.root.keywords {
             if !self.apply_keyword(keyword, value) {
                 return false;
             }
@@ -239,9 +252,13 @@ impl Validator {
 
     fn is_valid_for_schema(&self, value: &Value, schema_id: SchemaId) -> bool {
         // Evaluate all keywords and return on the first failed one
-        let schema = &self.schemas[schema_id.0];
+        let schema = if schema_id.0 == 0 {
+            &self.root
+        } else {
+            &self.schemas[schema_id.0]
+        };
 
-        for keyword in &self.keywords[schema.keywords.start..schema.keywords.end] {
+        for keyword in &schema.keywords {
             if !self.apply_keyword(keyword, value) {
                 return false;
             }
@@ -286,7 +303,7 @@ impl Validator {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Keyword {
     MaxLength { limit: usize },
     MinLength { limit: usize },
